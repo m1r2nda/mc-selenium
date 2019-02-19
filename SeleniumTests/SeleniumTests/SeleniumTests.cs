88b12c98000e72185e45aeeb2aa0495c1a90f84c
Tretiakova Marina <m1r2nda@skbkontur.ru>
2019-02-19 19:50:50 +0500

    Занятие 1. Мой первый Selenium тест (Википедия)

---
+++ SeleniumTests/SeleniumTests/SeleniumTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class SeleniumTests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized"); // браузер раскрывается на весь экран
+            driver = new ChromeDriver(options);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5)); //явные ожидания
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); //неявные ожидания
+        }
+
+        [Test]
+        public void MyFirstSeleniumTest()
+        {
+            driver.Navigate().GoToUrl("https://ru.wikipedia.org/");
+            IWebElement queryInput = driver.FindElement(By.Name("search"));
+            IWebElement searchButton = driver.FindElement(By.Name("go"));
+            queryInput.SendKeys("Selenium");
+            searchButton.Click();
+
+            Assert.That(driver.Title.Contains("Selenium — Википедия"), "Перешли на неверную страницу");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
