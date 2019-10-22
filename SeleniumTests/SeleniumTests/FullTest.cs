55a4f8b8bc3db10f0bd7f41af4a74896ac43023c
Tretiakova Marina <m1r2nda@skbkontur.ru>
2019-10-22 18:23:29 +0500

    Исправления. Готовый тест для 4 занятия

--- SeleniumTests/SeleniumTests/FullTest.cs
+++ SeleniumTests/SeleniumTests/FullTest.cs
@@ -30,6 +30,10 @@ namespace SeleniumTests
             //Основной тест
             driver.Navigate().GoToUrl("https://www.labirint.ru");
 
+            //Соглашаемся с политикой куки
+            wait.Until(ExpectedConditions.ElementExists(By.CssSelector(".js-cookie-policy-agree")));
+            (driver as IJavaScriptExecutor).ExecuteScript($"$('.js-cookie-policy-agree').click();");
+
             //Локаторы
             var booksMenu = By.CssSelector("[data-toggle='header-genres']"); // Ссылка "Книги" в шапке 
             var allBooks = By.CssSelector(".b-menu-second-container [href='/books/']");  //Ссылка "Все книги" в раскрывшемся меню
@@ -37,7 +41,7 @@ namespace SeleniumTests
             var issueOrder = By.XPath("(//a[contains(@class,'buy-link')][contains(@class,'btn-more')])[1]"); //Кнопка "Оформить" у первой книги
             var beginOrder = By.CssSelector("#basket-default-begin-order"); //Кнопка "Начать оформление"
             var chooseCourierDelivery = By.CssSelector("[data-gaid='cart_dlcourier']"); //Галочка для выбора курьерской доставки
-            var city = By.CssSelector("input[data-suggeststype='district']"); //Поле ввода названия города
+            var city = By.CssSelector(".js-dlform-wrap input[data-suggeststype^='district']"); //Поле ввода названия города
             var cityError = By.CssSelector("span.b-form-error"); //Локатор ошибки о неизвестном городе
             var suggestedCity = By.CssSelector(".suggests-item-txt"); //Локатор подсказки названия города
             var street = By.CssSelector(".js-dlform-wrap input[data-suggeststype='streets']"); //Название улицы
